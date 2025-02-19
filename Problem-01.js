ca6bfa98ebdb21484423d7f73819128010650ba1
Mamunk04 <khan110167@gmail.com>
2025-02-19 21:46:31 +0600

    1st problem solve

---
+++ Problem-01.js
@@ -0,0 +1,12 @@
+/*function signature/sample */
+function calculateVAT( price ) {
+    if(typeof amount !== "number" || amount < 0 ){
+        return "invalid"
+    }
+    const vatAmount = price * 7.5/100;
+    return vatAmount
+}
+
+const amount = 1500;
+const vat = calculateVAT(amount);
+console.log(vat)
